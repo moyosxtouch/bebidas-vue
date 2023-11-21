098d557705c8721c3f9e0d7193a5e6ca8884e3d9
moyo <moyosxtouch@gmail.com>
2023-11-21 13:58:10 -0600

    se modifico para guardar en el localStorage

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,9 +1,27 @@
+import { ref, watch, onMounted } from "vue";
 import { defineStore } from "pinia";
+import { useBebidasStore } from "./bebidas";
 export const useFavoritosStore = defineStore("favoritos", () => {
+  const bebidas = useBebidasStore();
+  const favoritos = ref([]);
+  watch(
+    favoritos,
+    () => {
+      sincronizarLocalStorage();
+    },
+    {
+      deep: true,
+    }
+  );
+  const sincronizarLocalStorage = () => {
+    localStorage.setItem("favoritos", JSON.stringify(favoritos.value));
+  };
+
   const handleClickFavorito = () => {
-    console.log("agregando...");
+    favoritos.value.push(bebidas.receta);
   };
   return {
+    favoritos,
     handleClickFavorito,
   };
 });
