ca84163d3cabc7b24c2f22c275589755439400fe
moyo <moyosxtouch@gmail.com>
2023-11-21 19:57:31 -0600

    se hicieron modificaciones en el modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -102,7 +102,7 @@ const formatearIngredientes = () => {
                   class="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
                   @click="favoritos.handleClickFavorito"
                 >
-                  Add to Favorites
+                  {{ modal.textoBoton }}
                 </button>
               </div>
             </DialogPanel>
